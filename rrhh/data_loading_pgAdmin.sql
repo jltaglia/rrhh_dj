4be912e9337efb8632a8cbf2105cb8a7b5ffd0e5
Julian Tagliarini <jltaglia2@gmail.com>
2022-10-08 22:28:58 -0300

    Rev 08-10-2022 22:28

---
+++ rrhh/data_loading_pgAdmin.sql
@@ -0,0 +1,39 @@
+COPY personal_categorias 
+FROM 'C:/Users/Public/Documents/csv/categorias.csv' 
+DELIMITER ';' 
+CSV HEADER;
+
+COPY personal_est_civil 
+FROM 'C:/Users/Public/Documents/csv/est_civil.csv' 
+DELIMITER ';' 
+CSV HEADER;
+
+COPY personal_eventos
+FROM 'C:/Users/Public/Documents/csv/eventos.csv' 
+DELIMITER ';' 
+CSV HEADER;
+
+COPY personal_parentescos
+FROM 'C:/Users/Public/Documents/csv/parentescos.csv' 
+DELIMITER ';' 
+CSV HEADER;
+
+COPY personal_provincias
+FROM 'C:/Users/Public/Documents/csv/provincias.csv' 
+DELIMITER ';' 
+CSV HEADER;
+
+COPY personal_tipo_doc
+FROM 'C:/Users/Public/Documents/csv/tipo_doc.csv' 
+DELIMITER ';' 
+CSV HEADER;
+
+COPY personal_personal
+FROM 'C:/Users/Public/Documents/csv/personal.csv' 
+DELIMITER ';' 
+CSV HEADER;
+
+COPY personal_localidades
+FROM 'C:/Users/Public/Documents/csv/localidades_reduc.csv' 
+DELIMITER ';' 
+CSV HEADER;
