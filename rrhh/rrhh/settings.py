23fc65ca96f8e691d040bbce29be0e1831bc1eae
Julian Tagliarini <jltaglia2@gmail.com>
2022-10-10 18:01:59 -0300

    Rev 10-10-2022 18:01

--- rrhh/rrhh/settings.py
+++ rrhh/rrhh/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'crispy_forms',
-    'crispy_bootstrap5',
     'personal.apps.PersonalConfig',
 ]
     # 'licencias.apps.LicenciasConfig',
@@ -133,6 +131,3 @@ MEDIA_URL = '/media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
-CRISPY_TEMPLATE_PACK = 'bootstrap5'
