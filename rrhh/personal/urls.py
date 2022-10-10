16f926381fbb837cf8359a4d97e39b1aca7b101e
Julian Tagliarini <jltaglia2@gmail.com>
2022-10-10 18:15:02 -0300

    Rev 10-10-2022 18:14

--- rrhh/personal/urls.py
+++ rrhh/personal/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('filter/', views.filter, name='filter'),
     path('create/', views.create, name='create'),
-    path('personal_detail/<int:pk>/', Detalle_Empleado.as_view(), name='show'),
+    path('personal_detail/<int:pk>/', Detalle_Empleado.as_view(), name='personal_detail'),
 ]
