602525bde50244bccd27fde3defe91fabc7dcb1b
Julian Tagliarini <jltaglia2@gmail.com>
2022-10-17 21:46:05 -0300

    Rev 17-10-2022 21:46

--- rrhh/personal/urls.py
+++ rrhh/personal/urls.py
@@ -1,17 +1,17 @@
 from django.urls import path
-from . import views
-from .views import (Ingreso_Filtro_Empleado,
+from personal.views import (Lista_Empleado,
+                    Ingreso_Filtro_Empleado,
                     Filtra_Empleado,
-                    Lista_Empleado,
-                    Edita_Empleado,
                     Nuevo_Empleado,
+                    Edita_Empleado,
                     Detalle_Empleado,
                     Baja_Empleado)
 
 urlpatterns = [
     path('', Lista_Empleado.as_view(), name='personal-index'),
     path('personal_filter/', Ingreso_Filtro_Empleado.as_view(), name='personal-filtro'),
-    path('personal_filtrado/', Filtra_Empleado.as_view(), name='personal-filtrar'),
+    path('personal_filtrado/<str:apellidos>/<str:nombres>/', Filtra_Empleado.as_view(), name='personal-filtrar'),
+    # path('personal_filtrado/', Filtra_Empleado.as_view(), name='personal-filtrar'),
     path('personal_create_form/', Nuevo_Empleado.as_view(), name='personal-alta'),
     path('personal_update_form/<int:pk>/', Edita_Empleado.as_view(), name='personal-editar'),
     path('personal_detail/<int:pk>/', Detalle_Empleado.as_view(), name='personal-detalle'),
