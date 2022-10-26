6131fc08a433279ea9aa225086c651f971134bda
Pablo Bellizzi <pbellizzi2@gmail.com>
2022-10-26 10:08:04 -0300

    imagenes

--- rrhh/rrhh/urls.py
+++ rrhh/rrhh/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("personal/", include('personal.urls')),
     path('admin/', admin.site.urls, name='admin'),
-]
+]  
 
 # if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns  += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
